e77561a4ef84c9f9c08cf7fdb0c19d4765977521
Desiders <abc_desi@proton.me>
2025-02-11 20:36:23 +0300

    Add database context

---
+++ SmartPreschool/Database.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using static SmartPreschool.Models;
+
+namespace SmartPreschool
+{
+    internal class Database
+    {
+        public class AppDbContext : DbContext
+        {
+            private string connectionString;
+
+            public AppDbContext(string connectionString)
+            {
+                this.connectionString = connectionString;
+            }
+
+            public DbSet<Child> Children { get; set; }
+            public DbSet<Group> Groups { get; set; }
+            public DbSet<Attendance> Attendance { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite(this.connectionString);
+            }
+        }
+    }
+}

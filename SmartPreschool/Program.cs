bae8b51483a6db191b7ade63f2bb732fe9b19525
Desiders <abc_desi@proton.me>
2025-02-12 20:39:27 +0300

    Remove unused imports in main

--- SmartPreschool/Program.cs
+++ SmartPreschool/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System.Threading;
-using Microsoft.Extensions.Options;
 using static SmartPreschool.Database;
 using static SmartPreschool.Logger;
 
